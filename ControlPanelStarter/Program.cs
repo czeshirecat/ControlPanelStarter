bea832dfe320b5981585fc4af67db609e956d316
czeshirecat <czeshirecat@hotmail.com>
2018-07-17 19:49:41 +0100

    Added from local project

---
+++ ControlPanelStarter/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Diagnostics;
+
+namespace ControlPanelStarter
+{
+  /// <summary>
+  /// Help solve the problem of Control Panel view settings not being stored if it's
+  /// pinned to start menu.
+  /// Simple solution is to build a regular application that can be installed
+  /// </summary>
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Process.Start("control.exe");
+    }
+  }
+}
---
+++ ControlPanelStarter/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ControlPanelStarter")]
+[assembly: AssemblyDescription("Optional starter for Windows 10 that effectively allows startup state to be saved")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("CzeshireSoft")]
+[assembly: AssemblyProduct("ControlPanel Starter Application")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("fe309f65-d53c-4f91-b564-61ac2091e642")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
